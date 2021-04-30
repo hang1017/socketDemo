58ecf26560092ecb015ccd67f34c06fd2c0c9688
chenshuhang <437132124@qq.com>
2021-04-30 11:00:39 +0800

    feat: node and web establish connection

--- socketH5/src/pages/index/index.tsx
+++ socketH5/src/pages/index/index.tsx
@@ -1,24 +1,36 @@
 import React, { FC, useEffect, useState } from 'react';
 import { List, InputItem } from 'antd-mobile';
+import io from 'socket.io-client';
 import './index.less';
 
 interface HomePageProps {
   location: any;
 }
 
+const random = () => Math.random().toString(36).substring(7);
+
 const HomePage: FC<HomePageProps> = ({ location }) => {
   const { name = '' } = location.query;
   const [inputValue, setInputValue] = useState<string>(''); // 输入框值
+  const [socket] = useState<any>(io('http://localhost:3000', { transports: ['websocket'] }));
   const [chatList, setChatList] = useState<any[]>([
     { id: '1', name: '明', type: 'text', message: '我叫小明' },
     { id: '2', name: '红', type: 'text', message: '我叫小红' },
     { id: '3', name: '明', type: 'text', message: '你好，很高兴认识你' },
   ]); // 聊天列表
+
+  console.log(socket);
+
+  useEffect(() => {
+    socket.on('message', (msg: any) => {});
+  }, [socket]);
+
   return (
     <div className="chatListStyle">
       <div className="chat">
         {chatList.map((item) => (
           <div
+            key={item?.id}
             className="item"
             style={{ justifyContent: item.name === name ? 'flex-end' : 'flex-start' }}
           >
@@ -37,6 +49,7 @@ const HomePage: FC<HomePageProps> = ({ location }) => {
             extra="发送"
             value={inputValue}
             onChange={(e) => setInputValue(e)}
+            // onExtraClick={sendMessage}
           />
         </List>
       </div>
